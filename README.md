3b8783b691fe4354277d0b7c1c593ba5b9c22cbb
Raymond Ma <raymondma44@gmail.com>
2019-12-07 19:13:57 -0800

    improved result text

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # PrediabetesQuiz
 Short and simple quiz to determine risk for prediabetes
 
+Why should I care about prediabetes?
+The sooner you know you have prediabetes, the sooner you can take action to reverse it and prevent type 2 diabetes.
+
+Resources:
+https://www.cdc.gov/prediabetes/takethetest/
+https://doihaveprediabetes.org/take-the-risk-test/
 https://www.cdc.gov/diabetes/prevention/pdf/Prediabetes-Risk-Test-Final.pdf
--- lib/result.dart
+++ lib/result.dart
@@ -9,9 +9,11 @@ class Result extends StatelessWidget {
   String resultPhrase() {
     String resultText;
     if (resultScore >= 5) {
-      resultText = 'HIGH RISK AND ARE LIKELY TO HAVE PREDIABETES';
+      resultText =
+          'HIGH RISK AND ARE LIKELY TO HAVE PREDIABETES. \n\n Based on your results, you’re likely to have prediabetes, but only your doctor can diagnose it for sure. \n';
     } else {
-      resultText = 'LOW RISK FOR PREDIABETES';
+      resultText =
+          'LOW RISK FOR PREDIABETES. \n\n Based on your results, you’re at low risk for prediabetes. Keep up the good work!\n';
     }
     return resultText;
   }
@@ -23,13 +25,13 @@ class Result extends StatelessWidget {
       children: <Widget>[
         Text(
           resultPhrase(),
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 36),
           textAlign: TextAlign.center,
         ),
         FlatButton(
           child: Text(
             'Retake Test!',
-            style: TextStyle(fontSize:24),
+            style: TextStyle(fontSize: 24),
           ),
           textColor: Colors.white,
           color: Colors.blue,
